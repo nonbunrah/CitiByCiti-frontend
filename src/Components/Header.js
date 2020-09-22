acd7d25afb2ccc1d24a3320668f22ccfd9601624
nonbunrah <rj.bamrah@gmail.com>
2020-09-21 19:42:16 -0700

    got started on frontend again

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Css/Header.css';
+
+function Header() {
+  return (
+    <header>
+        <h1 className='header'>Citi By Citi</h1>
+      </header>
+  )
+}
+
+export default Header;
